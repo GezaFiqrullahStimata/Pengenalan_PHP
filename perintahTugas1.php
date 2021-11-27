689c67278989b6f1a499bdba48c1c10238c37230
gezayu <gezayu@gmail.com>
2021-11-28 05:35:57 +0700

    penulisan php perintahTugas1

--- perintahTugas1.php
+++ perintahTugas1.php
@@ -4,11 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Belajar PHP</title>
 </head>
 <body>
-    <h1>
-        Selamat Datang <?php echo "Masukkan Nama Lengkap Anda"?>
-    </h1>
+    
+        <?php echo "<h1>Selamat Datang Masukkan Nama Lengkap Anda</h1>"?>
 </body>
 </html>
