454c5e4c45eb7fbd5c4acf3a84fa377a3b4ba160
gezayu <gezayu@gmail.com>
2021-11-29 21:37:58 +0700

    Tugas PHP2 : task 3

--- TugasPHP2_minggu4/latihan_php2.php
+++ TugasPHP2_minggu4/latihan_php2.php
@@ -6,8 +6,15 @@
 // }
 
 // <!-- #TASK 2 -->
-echo "<h1>Menggunakan Perulangan For</h1>";
-for($i=0 ; $i<5; $i++){
-    echo "Hai , Saya urutan ke - ".$i."</br>";
+// echo "<h1>Menggunakan Perulangan For</h1>";
+// for($i=0 ; $i<5; $i++){
+//     echo "Hai , Saya urutan ke - ".$i."</br>";
+// }
+
+// <!-- #TASK 3 -->
+$i = 0;
+while($i < 5){
+$i++;
+echo "Hai Dunia </br>";
 }
 ?>
