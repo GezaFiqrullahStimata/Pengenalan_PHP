54a707e5fb2802462596999f16a7298d594f31da
gezayu <gezayu@gmail.com>
2021-11-29 22:40:24 +0700

    new Tugas PHP2 : Task 5

--- TugasPHP2_minggu4/latihan_php2.php
+++ TugasPHP2_minggu4/latihan_php2.php
@@ -1,20 +1,49 @@
 
 <?php 
 // <!-- #TASK 1 -->
-// for($i=0 ; $i<5; $i++){
+// for($a=0 ; $a<5; $a++){
 //     echo "Hello world </br>";
 // }
 
 // <!-- #TASK 2 -->
 // echo "<h1>Menggunakan Perulangan For</h1>";
-// for($i=0 ; $i<5; $i++){
-//     echo "Hai , Saya urutan ke - ".$i."</br>";
+// for($b=0 ; $b<5; bi++){
+//     echo "Hai , Saya urutan ke - ".$b."</br>";
 // }
 
 // <!-- #TASK 3 -->
-$i = 0;
-while($i < 5){
-$i++;
-echo "Hai Dunia </br>";
+// $c = 0;
+// while($c < 5){
+// $c++;
+// echo "Hai Dunia </br>";
+// }
+
+// <!-- #TASK 4 -->
+// echo "<h1>Menggunakan Perulangan While</h1>";
+// $d = 0;
+// while($d <br 5){
+//     $d++;
+//     echo "Hai,ini Baris ke - ".$d."</br>";
+// }
+
+// <!-- #TASK 5 -->
+$e = 10;
+do{
+    $e++;
+    echo "Hai,Dunia </br>";
 }
+while($e < 5);
+//Jawaban : Karena kondisi didalam while bernilai false (yakni 10 kurang dari 5)
+// sehingga perintah didalam do , tetap dilakukan minimal sekali
+
+//Tetapi jika kondisinya bernilai true,maka perintah looping bisa dijalankan
+//sesuai inisialisasi nilai awal nya (yakni 0 kurang dari 5)
+//Contohnya seperti berikut :
+$f = 0;
+do{
+    $f++;
+    echo "Hai,Dunia";
+}
+while($f < 5)
+
 ?>
